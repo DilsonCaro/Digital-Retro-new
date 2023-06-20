f878f4decd743df275e45eef23ad07aeca9ab39e
DilsonCaro <114028958+DilsonCaro@users.noreply.github.com>
2023-06-19 21:11:09 -0400

    login funcionando

--- usuario/clases/mailer.php
+++ usuario/clases/mailer.php
@@ -18,7 +18,7 @@ class Mailer
 
         try {
             //Server settings
-            $mail->SMTPDebug = SMTP::DEBUG_SERVER; //SMTP::DEBUG_OFF   //Enable verbose debug output
+            // $mail->SMTPDebug = SMTP::DEBUG_SERVER; //SMTP::DEBUG_OFF   //Enable verbose debug output
             $mail->isSMTP(); //Send using SMTP
             $mail->Host = MAIL_HOST; //Set the SMTP server to send through
             $mail->SMTPAuth = true; //Enable SMTP authentication
